4b1f656eaad32f101a781c59412a4d357139d8ac
dvdMucci <85327117+dvdMucci@users.noreply.github.com>
2025-06-17 21:57:43 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 ssh -o StrictHostKeyChecking=no azureuser@20.64.144.178 <<HTML
 
-  git pull https://github.com/dvdMucci/github-Actions.git
+  git clone https://github.com/dvdMucci/github-Actions.git
 
 HTML
